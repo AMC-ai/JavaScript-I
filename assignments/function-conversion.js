cb9985d5b74de534ecbd1ffded0a4a72ac5f5586
AMC <53350942+AMC-ai@users.noreply.github.com>
2019-09-16 18:49:54 -0400

    finished func conversion

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -5,21 +5,46 @@
 // };
 // myFunction();
 
+let myFunction = () => {
+    console.log("Function was invoked!");
+};
+myFunction();
+console.log(myFunction);
+
 // let anotherFunction = function (param) {
-//   return param;
+//     return param;
 // };
 // anotherFunction("Example");
+
+
+let anotherFunction = (param) => {
+    return param;
+};
+anotherFunction("Example");
+
+console.log(anotherFunction);
 
 // let add = function (param1, param2) {
 //   return param1 + param2;
 // };
 // add(1,2);
 
+let add = (param1, param2) => {
+    return param1 + param2;
+};
+add(1, 2);
+console.log(add);
+
 // let subtract = function (param1, param2) {
 //   return param1 - param2;
 // };
 // subtract(1,2);
 
+let subtract = (param1, param2) => {
+    return param1 - param2;
+};
+subtract(1, 2);
+console.log(subtract);
 
 // Stretch
 
